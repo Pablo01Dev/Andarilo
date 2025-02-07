f8ee0e3ffbf853ad5702b781bb40153989899574
Pablo Guimarães <145077458+Pablo01Dev@users.noreply.github.com>
2025-02-07 13:16:48 -0300

    mobile version

--- src/models/Banner.jsx
+++ src/models/Banner.jsx
@@ -3,7 +3,6 @@ import { useInView } from 'react-intersection-observer';
 import lata from '../assets/images/lata.png';
 import styles from '../styles/Banner.module.css';
 
-
 function Banner() {
   const { ref, inView } = useInView({
     triggerOnce: true,
@@ -13,18 +12,16 @@ function Banner() {
   return (
     <div className={styles.bannerContainer}>
       <div className={styles.bannerOverlay}>
-        <img src={lata} alt="" />
+        <img src={lata} alt="Lata" className={styles.lataImage} />
       </div>
       <div ref={ref} className={`${styles.corpo} ${inView ? styles.visible : ''}`}>
         <h1>O Design comunica quando <br /> palavras não podem ser ditas.</h1>
         <h2>
-          As marcas que se destacam no crescimento são aquelas que apostam no design, 
-          com foco especial em uma identidade visual forte e marcante.
+          As marcas que mais crescem investem em design, priorizando uma identidade visual forte e marcante.
         </h2>
       </div>
     </div>
   );
-  
 }
 
 export default Banner;
