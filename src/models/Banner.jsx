58c8d7e2889b5bd00d32d24df6b878b87a19e5cf
Pablo Guimarães <145077458+Pablo01Dev@users.noreply.github.com>
2025-02-10 10:21:43 -0300

    ajustes

--- src/models/Banner.jsx
+++ src/models/Banner.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useInView } from 'react-intersection-observer';
+import Typed from 'typed.js';
 import lata from '../assets/images/lata.png';
 import styles from '../styles/Banner.module.css';
 
 function Banner() {
   const { ref, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.1,
+    triggerOnce: true,  // Isso garante que o efeito seja disparado apenas uma vez
+    threshold: 0.1,     // Define quando o efeito será disparado (10% visível)
   });
+
+  useEffect(() => {
+    if (inView) {
+      // Inicializa o Typed.js apenas quando o Banner entra em vista
+      const options = {
+        strings: [
+          "O Design comunica quando palavras não podem ser ditas."
+        ],
+        typeSpeed: 50, // Ajuste conforme necessário
+        startDelay: 0,
+        loop: false,   // Não vai repetir a animação
+        showCursor: false,
+      };
+
+      // Verifica se o Typed.js não foi inicializado já
+      const typedElement = document.getElementById("typed-output");
+      if (typedElement) {
+        new Typed(typedElement, options); // Aplica o efeito de digitação
+      }
+    }
+  }, [inView]);  // Dependência no inView, só dispara quando o banner entra na tela
 
   return (
     <div className={styles.bannerContainer}>
@@ -15,8 +37,8 @@ function Banner() {
         <img src={lata} alt="Lata" className={styles.lataImage} />
       </div>
       <div ref={ref} className={`${styles.corpo} ${inView ? styles.visible : ''}`}>
-        <h1>O Design comunica quando <br /> palavras não podem ser ditas.</h1>
-        <h2>
+        <h1 id="typed-output"></h1> {/* Aqui é onde o texto será digitado */}
+        <h2 className={inView ? styles.fadeIn : ''}> {/* Adicionando animação de fade-in ao h2 */}
           As marcas que mais crescem investem em design, priorizando uma identidade visual forte e marcante.
         </h2>
       </div>
