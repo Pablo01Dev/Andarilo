4cfecaa7d740b3fc3884235e1346f37739b276f9
Pablo01Dev <pablogomesdg@gmail.com>
2026-04-01 23:33:34 -0300

    ajustando caminho das imagens

--- src/models/QuemSomos.jsx
+++ src/models/QuemSomos.jsx
@@ -16,15 +16,11 @@ function QuemSomos() {
                 <div className={styles.personContent}>
                     <img src={Image1} alt="team" />
                     <p>
-                        O Andarilo é um estúdio de criação fundado por um trio de especialistas nas áreas de Identidade Visual, Desenvolvimento Web e Publicidade.<br></br>
+                        O Andarilo é um estúdio de publicidade fundado por especialistas nas áreas de Identidade Visual, Desenvolvimento Web e Publicidade.<br></br>
                         <br></br>
-                        Nossa missão vai além de executar projetos: nós construímos artefatos únicos. Unindo arte, código e estratégia de mercado, garantimos que cada entrega seja digna de admiração estética e impecável em sua performance.<br></br>
+                        Nossa missão vai além de executar projetos: nós unimos arte, algoritmos e estratégia de mercado, para criar soluções que são lembradas e convertem.<br></br>
                         <br></br>
-                        Afinal, a beleza e a eficiência precisam caminhar juntas. Não queremos que você precise escolher entre o belo e o funcional. <br></br>
-                        <br></br>
-                        Sabe aquela velha pergunta do biscoito: "vende mais porque é fresquinho ou é fresquinho porque vende mais?" Por aqui, nós preferimos os dois.<br></br>
-                        <br></br>
-                        Criamos soluções que são lembradas e convertem.
+                        Sabe aquela velha pergunta do biscoito: "vende mais porque é fresquinho ou é fresquinho porque vende mais?" Por aqui, nós não escolhemos um lado. Preferimos os dois: o visual que  converte e faz ser lembrado.
                     </p>
 
                 </div>
